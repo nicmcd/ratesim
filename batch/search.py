0dcfc4df2dffa8a2311cb500dab88e17e28923c9
Nic McDonald <n.mcdonald83@gmail.com>
2016-02-28 20:29:05 -0800

    moved stats parsing into its own module

--- batch/search.py
+++ batch/search.py
@@ -10,34 +10,7 @@ import sys
 import taskrun
 
 from common import *
-
-
-def parse(filename, verbose):
-  strmap = {
-    'bandwidth overhead': 'bw',
-    '99%ile latency': '2-9s',
-    '99.9%ile latency': '3-9s',
-    '99.99%ile latency': '4-9s',
-    '99.999%ile latency': '5-9s'
-  }
-
-  stats = {}
-  sect = None
-  with open(filename, 'r') as fd:
-    for line in fd:
-      line = line.strip()
-      if line.startswith('Section #'):
-        sect = int(line[len('Section #'):])
-        assert sect not in stats
-        stats[sect] = {}
-        if verbose:
-          print('section {0}'.format(sect))
-      elif line.find('=') > -1:
-        stat, value = (x.strip() for x in line.split('='))
-        if verbose:
-          print('stat \'{0}\' -> \'{1}\''.format(stat, value))
-        stats[sect][strmap[stat]] = float(value)
-  return stats
+from stats import *
 
 
 def main(args):
@@ -65,7 +38,7 @@ def main(args):
         print('file \'{0}\' matched'.format(ff))
 
       # parse the file
-      stats = parse(ff, args.verbose)
+      stats = parseStats(ff, args.verbose)
 
       # compare against current best
       if stats[args.sect][args.stat] < bestValue:
---
+++ batch/stats.py
@@ -0,0 +1,26 @@
+def parseStats(filename, verbose=False):
+  strmap = {
+    'bandwidth overhead': 'bw',
+    '99%ile latency': '2-9s',
+    '99.9%ile latency': '3-9s',
+    '99.99%ile latency': '4-9s',
+    '99.999%ile latency': '5-9s'
+  }
+
+  stats = {}
+  sect = None
+  with open(filename, 'r') as fd:
+    for line in fd:
+      line = line.strip()
+      if line.startswith('Section #'):
+        sect = int(line[len('Section #'):])
+        assert sect not in stats
+        stats[sect] = {}
+        if verbose:
+          print('section {0}'.format(sect))
+      elif line.find('=') > -1:
+        stat, value = (x.strip() for x in line.split('='))
+        if verbose:
+          print('stat \'{0}\' -> \'{1}\''.format(stat, value))
+        stats[sect][strmap[stat]] = float(value)
+  return stats
