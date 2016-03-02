b285ca18959f727f4ef6b13dbfc08ddde72804af
Nic McDonald <n.mcdonald83@gmail.com>
2016-03-02 10:18:42 -0800

    updated penalty formula

--- batch/penalty.py
+++ batch/penalty.py
@@ -1,9 +1,17 @@
 def computePenalty(stats):
-  bw_ov = stats[3]['bw']
-  lat_ov = stats[2]['4-9s'] - 500
+  # this takes a weighted average of the latencies
+  #  currently all three sections have the same weight
+  lat_ov = sum(((stats[1]['4-9s'] - 500) * 1.0,
+                (stats[2]['4-9s'] - 500) * 1.0,
+                (stats[3]['4-9s'] - 500) * 1.0)) / 3
 
+  # this takes the a weighted maximum of the bandwidths
+  #  currently the 3rd section has a 2x weight because during this time the
+  #  senders are sending at the same rate
+  bw_ov = max((stats[1]['bw'] * 1.0,
+               stats[2]['bw'] * 1.0,
+               stats[3]['bw'] * 2.0))
 
-  bw_pen = bw_ov
-  lat_pen = lat_ov / 100
-
-  return bw_pen + lat_pen
+  # 'coeff' nanoseconds is worth 1 phits/sec of bandwidth
+  coeff = 100.0
+  return lat_ov + coeff * bw_ov
