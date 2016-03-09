1d54d6de8ae3c1ea062aca312a432db3e13bf7c4
Nic McDonald <n.mcdonald83@gmail.com>
2016-03-08 16:19:09 -0800

    updated penalty function with 75 coeff and max 99.99th latency

--- batch/penalty.py
+++ batch/penalty.py
@@ -1,9 +1,9 @@
 def computePenalty(stats):
-  # this takes a weighted average of the latencies
+  # this takes a weighted maximum of the latencies
   #  currently all three sections have the same weight
-  lat_ov = sum(((stats[1]['4-9s'] - 500) * 1.0,
+  lat_ov = max(((stats[1]['4-9s'] - 500) * 1.0,
                 (stats[2]['4-9s'] - 500) * 1.0,
-                (stats[3]['4-9s'] - 500) * 1.0)) / 3
+                (stats[3]['4-9s'] - 500) * 1.0))
 
   # this takes the a weighted maximum of the bandwidths
   #  currently the 3rd section has a 2x weight because during this time the
@@ -13,5 +13,5 @@ def computePenalty(stats):
                stats[3]['bw'] * 2.0))
 
   # 'coeff' nanoseconds is worth 1 phits/sec of bandwidth
-  coeff = 100.0
+  coeff = 75.0
   return lat_ov + coeff * bw_ov
